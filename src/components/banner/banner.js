1cc216ddb8a5479ed69cd3beb04214255e01cdab
Zac Brammer <zacharyj.brammer@gmail.com>
2022-05-22 15:29:42 -0400

    Banner Styling
    Created styling for the banner so the text and height of the banner match the figma

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -3,10 +3,12 @@ import './banner.css';
 
 function Banner() {
     return (
-        <div className="banner">
-            <h1 className='top-text'>Welcome to AI Camp!</h1>
-            <h1 className='bottom-text'>What are you waiting for? Start your AI journey now!</h1>
-        </div>
+        <header className='banner'>
+            <div className='text-section'>
+                <h1 className='top-text'>Welcome to AI Camp!</h1>
+                <h1 className='bottom-text'>What are you waiting for? Start your AI journey now!</h1>
+            </div>
+        </header>
     );
 }
 

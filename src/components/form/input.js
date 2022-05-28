b2245e6ce0a9bd8e25135d05c4c1fc81dcc23489
Alexander Zhou <alexzhou3999@gmail.com>
2022-05-27 20:27:53 -0700

    slight tweaks to input pt 2.

--- src/components/form/input.js
+++ src/components/form/input.js
@@ -6,24 +6,23 @@ function Input({placeHolder, label, type, value, onChange, isValid, errorText, c
 
     if (isValid === 'false') {
         error = errorText;
-        style = 'invalid-border';
+        style += ' invalid-border';
     }
 
     return (
-        <>
+        <div className={`${className}`}>
             <label className='label' htmlFor={id}>{label}</label>
-
             <input type={type} 
             onChange={(e) => {
 				if (onChange) onChange(e.target.value)
 			} }
             placeholder={placeHolder}
-            className={`${style} ${className}`}
+            className={style + ' w-full md:w-9/12'}
             value={value}
 			id={id}>
             </input>
             <span className='invalid-text'>{error}</span>
-        </>
+        </div>
     );
 }
 

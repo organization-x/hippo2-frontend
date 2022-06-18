cd41c39c3a13314ba89bfb225d636d41f3114046
Shigmah <du.leo.x@gmail.com>
2022-06-17 22:52:24 -0400

    final changes

--- src/components/batch-select/batchSelect.test.js
+++ src/components/batch-select/batchSelect.test.js
@@ -1,9 +1,9 @@
 import BatchSelect from './batchSelect.js';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 test('batch select test', () => {
   const batch_no = -1;
-  const batchID = '';
   const testData = {
     "id": "af83050f-121c-4851-9ba6-78a1ae70ab48",
     "name": "Test Course",
@@ -102,17 +102,11 @@ test('batch select test', () => {
   };
 
     render(
-      <BatchSelect 
-        batchData={testData} 
-        onChange={
-                   (batch_no, batchID) => { 
-                    selectBatchNo(batch_no);
-                    selectBatchID(batchID); 
-                    console.log(batch_no);
-                    console.log(batchID);
-                   } 
-                }
-        batchIndex={batch_no}/>
+        <BrowserRouter>
+            <BatchSelect 
+                batchData={testData} 
+                batchIndex={batch_no}/>
+        </BrowserRouter>
     )
     const seats_left = screen.getByRole('button', { name: '20 LEFT' });
     const open = screen.getByRole('button', { name: 'OPEN' });
@@ -120,10 +114,10 @@ test('batch select test', () => {
     const back = screen.getByRole('button', { name: 'Back' });
     const next = screen.getByRole('button', { name: 'Next' });
 
-    expect(seats_left.className.includes('btn bg-yellow my-4 w-1/4 md:w-11/12 h-12 md:h-8 txt-white button active')).toBeTruthy();
-    expect(open.className.includes('btn bg-green my-4 w-1/4 md:w-11/12 h-12 md:h-8 txt-white button active')).toBeTruthy();
-    expect(sold_out.className.includes('btn bg-red my-4 w-1/4 md:w-11/12 h-12 md:h-8 txt-white button active')).toBeTruthy();
-    expect(back.className.includes('btn bg-gray w-full md:w-1/4 h-12 mb-6 md:my-3 txt-white button active')).toBeTruthy();
-    expect(next.className.includes('btn bg-green w-full md:w-1/2 h-12 md:my-3 txt-white button active')).toBeTruthy();
+    expect(seats_left.className.includes('btn bg-yellow my-4 w-1/4 text-center lg:w-11/12 p-2 lg:p-1 inline-block txt-white button active')).toBeTruthy();
+    expect(open.className.includes('btn bg-green my-4 w-1/4 text-center lg:w-11/12 p-2 lg:p-1 inline-block txt-white button active')).toBeTruthy();
+    expect(sold_out.className.includes('btn bg-red my-4 w-1/4 text-center lg:w-11/12 p-2 lg:p-1 inline-block txt-white button active')).toBeTruthy();
+    expect(back.className.includes('btn bg-gray w-full lg:w-1/4 h-12 mb-6 lg:my-3 txt-white button active')).toBeTruthy();
+    expect(next.className.includes('btn bg-green w-full lg:w-1/2 h-12 lg:my-3 txt-white button active')).toBeTruthy();
 });
 

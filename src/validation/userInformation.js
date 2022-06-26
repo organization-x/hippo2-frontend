0db9fd4a6d3b3518e7d5088c37941d7aace57828
Alexander Zhou <alexzhou3999@gmail.com>
2022-06-25 20:21:20 -0700

    new invite page

--- src/validation/userInformation.js
+++ src/validation/userInformation.js
@@ -2,17 +2,17 @@ import { nonempty, object, pattern, string, validate } from "superstruct";
 import formatFormErrors from "./formatFormErrors";
 
 const StudentInformation = object({
-	firstName: pattern(nonempty(string()), /^[a-zA-Z]*$/),
-	lastName: pattern(nonempty(string()), /^[a-zA-Z]*$/),
 	email: pattern(nonempty(string()), /^\S+@\S+\.\S+$/),
+	fName: pattern(nonempty(string()), /^[a-zA-Z]*$/),
+	lName: pattern(nonempty(string()), /^[a-zA-Z]*$/),
 	type: pattern(nonempty(string()), /^(student|parent)$/),
 	phone: pattern(nonempty(string()), /^\+?1?\d{9,15}$/),
 	dob: pattern(nonempty(string()), /^(0?[1-9]|1[0-2])\/(19|20)\d{2}$/)
 });
 const ParentInformation = object({
-	firstName: pattern(nonempty(string()), /^[a-zA-Z]*$/),
-	lastName: pattern(nonempty(string()), /^[a-zA-Z]*$/),
 	email: pattern(nonempty(string()), /^\S+@\S+\.\S+$/),
+	fName: pattern(nonempty(string()), /^[a-zA-Z]*$/),
+	lName: pattern(nonempty(string()), /^[a-zA-Z]*$/),
 	phone: pattern(nonempty(string()), /^\+?1?\d{9,15}$/),
 	type: pattern(nonempty(string()), /^(student|parent)$/)
 });

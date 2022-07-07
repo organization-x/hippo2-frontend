4447cddd636bc90928f2e9898c06b04464335d67
18anguyen9 <avn001@ucsd.edu>
2022-07-06 17:53:57 -0700

    resolved comments

--- src/pages/dashboard/alertBanner.js
+++ src/pages/dashboard/alertBanner.js
@@ -1,39 +1,41 @@
 import React from "react";
-import { useState,useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { useFlashMsg } from "../../services/flashMsg";
 import { useAuth } from "../../services/authentication";
 import baseUrl from '../../apiUrls';
 
 
 function AlertBanner(){
     const auth = useAuth();
-    const [isParent,setIsParent] = useState(false);
+    const isParent = auth.user.type === 'PARENT';
     const [completed,setCompleted] = useState(true);
+    const {flashMsg} = useFlashMsg(); 
+    const flashMsgRef = useRef(flashMsg).current;
+
     useEffect(() => {
-        if (auth.user.type==='PARENT'){
-            setIsParent(true);
-        }
         const url =  baseUrl+'/api/v1/users/'+auth.user.id+'/tasks/';
         auth.autoAuthReq(url,{method:'GET'})
 			.then(data => {
                 setCompleted(data.data.completed);
-			});
-	  }, [auth]);
+			}).catch(err => {
+                // API request was not successful
+                flashMsgRef('error', 'Unable to load Completed Tasks');
+            });
+      }, [auth,flashMsgRef]);
     
     return (
         <>
-            {completed ?
+            {!completed ?
                 <div className="bg-red-500 h-max text-white text-center py-6">
                     {isParent ? 
                         <div>
                             <p>One or more of your children have incomplete tasks in their To Do List.</p>
                             <p> Click here to complete them so you can help your child prepare for their course!</p>
-                        </div>:null
-                    }
-                    {!isParent ? 
+                        </div>:
                         <div>
                             <p>You have incomplete tasks in your To Do List.</p>
                             <p> Click here to complete them now so you can gain access to your couse materials!</p>
-                        </div>:null
+                        </div>
                     }
                 </div>:null
             }

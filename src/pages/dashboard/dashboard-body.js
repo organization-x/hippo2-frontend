792ccb6ac88afbc21511964879aa55b1b2ed22f7
MooCowGalaxy <64293406+MooCowGalaxy@users.noreply.github.com>
2022-07-07 11:55:34 -0700

    make dashboard home page course details page

--- src/pages/dashboard/dashboard-body.js
+++ src/pages/dashboard/dashboard-body.js
@@ -1,12 +1,12 @@
 import React from "react";
-import DashboardHome from "./dashboard-home"
+import DashboardCourseDetails from "./screens/courseDetails";
 import AccountSettings from "./account-settings";
 import ToDo from "./screens/ToDo";
 
-function DashboardBody({page}){
+function DashboardBody({page}) {
         switch(page) {
             case 'dashboard-home':
-                return (<DashboardHome />);
+                return (<DashboardCourseDetails />);
             case 'To-Do':
                 return (<ToDo />);
             case 'account-settings':

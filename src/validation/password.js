5e549218c12e893413ad7d22b62b4aa6c5f2efd0
Alexander Zhou <alexzhou3999@gmail.com>
2022-07-08 14:13:45 -0700

    prep linter upgrade

--- src/validation/password.js
+++ src/validation/password.js
@@ -5,21 +5,21 @@ import formatFormErrors from "./formatFormErrors";
 const Password = size(nonempty(string()), 7, Infinity);
 
 const messages = {
-    pattern: {
-        default: 'This field is invalid'
-    },
-    nonempty: {
-        default: 'This field is required'
-    },
+	pattern: {
+		default: 'This field is invalid'
+	},
+	nonempty: {
+		default: 'This field is required'
+	},
 	size: {
 		default: 'Password length must be more than 7'
 	}
 };
 
 function validatePassword(password) {
-    const [err, vPassword] = validate(password, Password);
-    const formattedErr = formatFormErrors(err, messages, true);
-    return [formattedErr, vPassword];
+	const [err, vPassword] = validate(password, Password);
+	const formattedErr = formatFormErrors(err, messages, true);
+	return [formattedErr, vPassword];
 }
 
 export default validatePassword;

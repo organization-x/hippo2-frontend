354007a431515a2d2bccf254355334997d2d4ed4
Shigmah <du.leo.x@gmail.com>
2022-07-14 16:18:04 -0400

    fixes

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -1,19 +1,16 @@
 //for left and right width, use tailwindcss standard classses (https://tailwindcss.com/docs/width)
-function page(leftChildren, rightChildren, leftWidth, rightWidth) {
+//for maxWidth, use tailwindcss standard classses (https://tailwindcss.com/docs/max-width)
+function Page(leftChildren, rightChildren, leftWidth, rightWidth, maxWidth) {
 	return (
-		<div className="container max-w-3xl flex flex-wrap mx-auto p-4 auth">
-			<div className={`flex-none md:flex-initial w-full md:w-${leftWidth} p-5 text-white bg-green rounded-t-xl md:rounded-l-xl md:rounded-none`}>
-				<div className='mb-4 mx-4'>
-					{leftChildren}
-				</div>
+		<div className={`container max-w-${maxWidth} flex flex-wrap mx-auto my-10 md:rounded-l-xl md:rounded-none md:rounded-r-xl overflow-hidden rounded-t-xl rounded-b-xl bg-green`}>
+			<div className={`w-full md:w-${leftWidth} p-5 text-white bg-green p-7`}>
+				{leftChildren}
 			</div>
-			<div className={`flex-none md:flex-initial w-full md:w-${rightWidth} py-5 px-8 bg-white rounded-b-xl md:rounded-r-xl md:rounded-none`}>
-				<div className='mb-4'>
-					{rightChildren}
-				</div>
+			<div className={`w-full md:w-${rightWidth} py-5 px-8 bg-white p-7`}>
+				{rightChildren}
 			</div>
 		</div>
 	);
 }
 
-export default page;
+export default Page;

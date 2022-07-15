4195fab05432676c36a8f4147dd80629e6c6b6d8
Your Name <nathacutlan@gmail.com>
2022-07-15 15:14:41 -0400

    added human component

---
+++ src/components/human/human.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import './human.css';
+
+function Human() {
+	const [human, setHuman] = useState({ 'name': 'Name', 'role': 'Role', 'imgUrl': '' });
+	useEffect(() => {
+		const humans = [
+			{ 'name': 'dummy1', 'role': 'Backend Engineer', 'imgUrl': 'https://www.jbrhomes.com/wp-content/uploads/blank-avatar.png' },
+			{ 'name': 'dummy2', 'role': 'Designer', 'imgUrl': 'https://barryburnett.net/wp-content/uploads/2018/03/Blank-Avatar-Man-in-Suit.jpg' },
+			{ 'name': 'dummy3', 'role': 'Tech Lead', 'imgUrl': 'https://rocksolidpaintingco.com/wp-content/uploads/2019/10/Blank-avatar.png' },
+			{ 'name': 'dummy4', 'role': 'Data Scientist', 'imgUrl': 'https://freesvg.org/img/generic-avatar.png' },
+			{ 'name': 'dummy5', 'role': 'Frontend Engineer', 'imgUrl': 'https://pbs.twimg.com/media/B5PP-IIIMAA-qem.png' },
+		];
+		setHuman(humans[Math.floor(Math.random() * humans.length)]);
+	}, []);
+
+	return (
+		<div className="flex flex-col items-center">
+			<img src={human.imgUrl} alt="" className="mb-4 human_img"/>
+			<h1>Hi! I'm {human.name}, {human.role} at AI Camp.</h1>
+			<h1>Let's get started</h1>
+		</div>
+	);
+}
+
+export default Human;

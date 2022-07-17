42e1c2f4f0e1342f9d7e8fb9a6347b56a57a418e
Ray Cai Flanagan <hjkareus@gmail.com>
2022-07-17 14:26:31 -0400

    comit

--- src/components/ttlogo/ttlogo.js
+++ src/components/ttlogo/ttlogo.js
@@ -7,42 +7,27 @@ function TTLogo(){
     const [displayTextBox, updateDisplay] = useState(false);
 
     return (
-        <div className="768px or hidden md:block ttlogo-main-container">
+        <div className="ttlogo-main-container">
             
-            <a className="tt-logo">
-				<img src={TTLogoSVG} alt="logo" onMouseEnter={() => updateDisplay(true)} onMouseLeave={() => updateDisplay(false)}  href="https://www.ai-camp.org/"/>
+            <a href="https://www.ai-camp.org/" className="tt-logo">
+				<img src={TTLogoSVG} alt="logo"/>
             </a>
 
-            {
-                displayTextBox ? (
-                <div className="ttlogo-text-box-container">
-                    <div className="hippo-logo-container">
-                        <img src={HippoSVG} alt="hippo-logo" className="ttlogo-text-box-hippo float-right"/>
-                    </div>
-                
-                    <div className="ttlogo-text-box-left">
-                        <p className="ttlogo-text-box-top">
-                            This system was built entirely by Team Tomorrow, AI Camp’s team of teen developers, in a project called hippo2!    
-                        </p>
-                        
-                        <div className="ttlogo-text-box-bottom">
-                            <div className="tt-intro flex">
-                                <h1>Product Manager:</h1>
-                                <p>Sri</p>
-                            </div>
-                            <div className="tt-intro flex">
-                                <h1>Product Designer:</h1>
-                                <p>Bernice</p>
-                            </div>
-                            <div className="tt-intro flex">
-                                <h1>Software Developers:</h1>
-                                <p>Jackson, Alex Z</p>
-                            </div>
-                        </div>
-                    </div>
+            <div className="ttlogo-text-box-container">
+                <div className="hippo-logo-container">
+                    <img src={HippoSVG} alt="hippo-logo" className="ttlogo-text-box-hippo float-right"/>
                 </div>
-                ) : null
-            }
+                
+                <div className="ttlogo-text-box-left">
+                    <p className="ttlogo-text-box-top text">
+                        This system was built entirely by Team Tomorrow, AI Camp’s team of teen developers, in a project called hippo2!    
+                    </p>
+                    
+                    <p><span>Product Manager: </span> Sricharan Guddanti</p>        
+                    <p><span>Product Designer: </span> Bernice Lau</p>        
+                    <p><span>Software Developers: </span> Jackson Choyce, Alex Zhou</p>        
+                </div>
+            </div>
 
             
         </div>

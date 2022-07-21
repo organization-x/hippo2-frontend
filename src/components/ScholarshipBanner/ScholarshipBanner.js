6e5a6b2161bfa72728449af0f52d2d235e35d3f8
TheFeesh <43019971+TheFeesh@users.noreply.github.com>
2022-07-20 18:12:39 -0700

    css simplification to scholarship banner

--- src/components/ScholarshipBanner/ScholarshipBanner.js
+++ src/components/ScholarshipBanner/ScholarshipBanner.js
@@ -2,7 +2,7 @@ function ScholarshipBanner({ isGeneral }) {
 
 	if (isGeneral) {
 		return (
-			<div className="flex flex-col top-0 width-full flex bg-main_green justify-center mx-auto p-5">
+			<div className="w-full bg-main_green p-5">
 				<h1 className="font-bold mt-10 text-4xl mb-8 text-center">General Questions</h1>
 				<p className="mx-20 mb-10 text-base text-center">
                 Please fill out a few questions in order for us to learn more about you. Don't worry, your responses will <b>not</b> affect scholarship decisions.
@@ -11,7 +11,7 @@ function ScholarshipBanner({ isGeneral }) {
 		);
 	} else {
 		return (
-			<div className="flex flex-col top-0 width-full flex bg-main_green justify-center mx-auto p-5">
+			<div className="w-full bg-main_green p-5">
 				<h1 className="font-bold mt-10 text-4xl mb-8 text-center">Application Questions</h1>
 				<p className="mx-20 mb-10 text-base text-center">
                     Please fill out a few questions in order for us to get a better sense of who you are! Responses <b>will be </b> taken into consideration when evaluating scholarship applicants.
